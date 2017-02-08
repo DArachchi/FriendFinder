2e7d9f5c46835bf36cb79ebeed2dcafaeea2d78c
Dino Arachchi <Dino Arachchi>
2017-02-08 17:57:35 -0600

    Added external file for friends data

--- server.js
+++ server.js
@@ -5,86 +5,19 @@ var path = require("path");
 var app = express();
 var PORT = process.env.PORT || 8080;
 
-var friends = [
-	{
-		"name":"Ahmed",
-		"photo":"https://media.licdn.com/mpr/mpr/shrinknp_400_400/p/6/005/064/1bd/3435aa3.jpg",
-		"scores":
-			[
-			5,
-			1,
-			4,
-			4,
-			5,
-			1,
-			2,
-			5,
-			4,
-			1
-			]
-	},
-	{
-		"name": "Jacob Deming",
-		"photo": "https://pbs.twimg.com/profile_images/691785039043022849/oWsy8LNR.jpg",
-		"scores":
-			[
-			4,
-			2,
-			5,
-			1,
-			3,
-			2,
-			2,
-			1,
-			3,
-			2
-			]
-	},
-	{
-		"name": "Jeremiah Scanlon",
-		"photo": "https://avatars2.githubusercontent.com/u/8504998?v=3&s=460",
-		"scores":
-			[
-			5,
-			2,
-			2,
-			2,
-			4,
-			1,
-			3,
-			2,
-			5,
-			5
-			]
-	},
-	{
-		"name": "Louis T. Delia",
-		"photo": "https://pbs.twimg.com/profile_images/639214960049000449/lNCRC-ub.jpg",
-		"scores":
-			[
-			3,
-			3,
-			4,
-			2,
-			2,
-			1,
-			3,
-			2,
-			2,
-			3
-			]
-	}
-]
 
+
+var data = require("./app/data/friends.js");
+var friendsList = data.object.friends;
 var userData;
 var matchIndex;
 
 function checkMatch() {
 	var lowestDifference = 100;
-	for(var i=0;i<friends.length;i++) {
+	for(var i=0; i<friendsList.length; i++) {
 		var totalDifference = 0;
-		for(var j=0;j<userData.length;j++) {
-			totalDifference += Math.abs(userData[j]-friends[i].scores[j]);
+		for(var j=0; j<userData.length; j++) {
+			totalDifference += Math.abs(userData[j]-friendsList[i].scores[j]);
 		}
 		if (totalDifference < lowestDifference) {
 			matchIndex = i;
@@ -92,7 +25,6 @@ function checkMatch() {
 		}
 	}
 }
-
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
@@ -113,10 +45,10 @@ app.get("/api/friends", function(req, res) {
 });
 
 app.post("/api/friends", function(req, res) {
-	friends.push(req.body);
 	userData = req.body.scores;
 	checkMatch();
-	res.json(friends[matchIndex]);
+	friendsList.push(req.body);
+	res.json(friendsList[matchIndex]);
 });
 
 
